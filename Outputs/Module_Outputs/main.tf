40507bb9edd9d4e54be7e9e6997c88f5bcffa08a
Paul CHeyne <paulc75@hotmail.com>
2024-11-17 11:02:46 +0000

    Output testing updates

---
+++ Outputs/Module_Outputs/main.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source          = "../../../AWS/Modules/Vpc"
+  vpc_cidr_block  = "10.0.0.0/16"
+  private_subnets = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+  tags = {
+    Name = "Testing Outputs"
+  }
+
+}
